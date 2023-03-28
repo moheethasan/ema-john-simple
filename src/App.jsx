44ce0a6b099df4cc7c722ba657be6d0ad21286fe
majesty-01 <moheethasan4@gmail.com>
2023-03-28 15:42:08 +0600

    added navigation and card

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Shop></Shop>
+    </div>
+  )
+}
+
+export default App
